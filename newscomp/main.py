6b1c7cd6ab37bcac7178e9dbb0b2ade866937bb0
Chris <christoph.kralj@gmail.com>
2015-08-02 20:05:29 +0200

    Concordance method works

--- newscomp/main.py
+++ newscomp/main.py
@@ -1,9 +1,20 @@
+# Web stuff
 from bs4 import BeautifulSoup as bs
 from urllib import request
 import lxml
+
+# NLP stuff
+from nltk.corpus import stopwords
 import re
 import nltk as nlp
+
+# My own classes
 import web
+
+# For the concordance output
+import io
+from contextlib import redirect_stdout
+
 
 def basic_statistics(text, fd):
     """Computes basic metrics and statistics for a article frequency distribution
@@ -27,29 +38,46 @@ def basic_statistics(text, fd):
 
 # NLP
 
-# Get article
 url_zeit = "http://www.zeit.de/gesellschaft/zeitgeschehen/2015-07/malaysia-airlines-mh370-wrackteil-frankreich-toulous"
 url_krone = "http://www.krone.at/Welt/Grosse_Zuversicht._dass_Wrackteil_von_MH370_stammt-Verschollene_Boeing-Story-465019"
 
+# Initialize variables and classes
+# My classes
 web = web.FetchArticle()
 
-zt = web.get_article(url_zeit, "zeit")
-kt = web.get_article(url_krone, "krone")
+# Get all german stopwords and , and . 
+g_stop = stopwords.words("german")
+g_stop.append(",")
+g_stop.append(".")
 
-ztokens = zt.tokens
-ktokens = kt.tokens
+# German stemmer
+snow = nlp.stem.snowball.GermanStemmer(ignore_stopwords=True)
 
-zfd = nlp.FreqDist(ztokens)
-kfd = nlp.FreqDist(ktokens)
+# Get the article text
+zeit_text = web.get_article(url_zeit, "zeit")
+krone_text = web.get_article(url_krone, "krone")
 
+# Remove stopwords and tokenize
+zt = [w.lower() for w in zeit_text.tokens if w not in g_stop]
+kt = [w.lower() for w in krone_text.tokens if w not in g_stop]
+
+# Tokenize words
+zt = [snow.stem(w) for w in zt]
+kt = [snow.stem(w) for w in kt]
+
+zfd = nlp.FreqDist(zt)
+kfd = nlp.FreqDist(kt)
+
+# Basic statistics
 zstat = basic_statistics(zt,zfd)
 kstat = basic_statistics(kt,kfd)
 
-zvocab = set(ztokens)
-kvocab = set(ktokens)
+# Vocabulary
+zvocab = set(zt)
+kvocab = set(kt)
 common_vocab = zvocab.intersection(kvocab)
 
-tokens = {"Zeit":ztokens, "Krone":ktokens}
+tokens = {"Zeit":zt, "Krone":kt}
 
 cfd = nlp.ConditionalFreqDist(
         (news, word)
@@ -57,13 +85,37 @@ cfd = nlp.ConditionalFreqDist(
         for word in tokens[news]
         )
 
+# Word length comparrison
+zwl = nlp.FreqDist([len(w) for w in zt])
+kwl = nlp.FreqDist([len(w) for w in kt])
 
+wordlength = {"Zeit": zwl, "Krone": kwl}
 
+cwl = nlp.ConditionalFreqDist(
+        (news, length)
+        for news in ["Zeit","Krone"]
+        for length in wordlength[news]
+        )
 
+# Generates list for concordance function
+def word_concordance(word, text):
+    """Prints concordance for the word
 
+    :word: word/wordstem 
+    :text: nltk text
+    :returns: concordance string
 
-# Distribution of word length
-# fd_wl = nlp.FreqDist([len(w) for w in zfd])
+    """
+
+    concordance = io.StringIO()
+
+    words = set([w for w in zeit_text.tokens if re.findall("\\b\\w*"+word+"\\w*\\b",w)])
+
+    with redirect_stdout(concordance):
+        for w in words:
+            text.concordance(w)
+
+    return concordance.getvalue()
 
 # Plotting
 
